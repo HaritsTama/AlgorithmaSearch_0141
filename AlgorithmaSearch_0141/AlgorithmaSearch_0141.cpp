97d5846ee4cc6a71d55560e8cb311088f23bdde7
HaritsTama <nayotama1818@gmail.com>
2024-04-05 10:38:41 +0700

    menyelesaikan proses do while dan menambahkan simple if pada line 47

--- AlgorithmaSearch_0141/AlgorithmaSearch_0141.cpp
+++ AlgorithmaSearch_0141/AlgorithmaSearch_0141.cpp
@@ -36,8 +36,23 @@ void LinearSearch()
 	do
 	{
 		// Menerima nomor untuk dicari
-		while (i < n)
-	}
+		cout << "\nEnter the element you want to search "; //step 1
+		cin >> item;
+
+		ctr = 0;
+		i = 0; //step 2
+		while (i < n) //step 3
+		{
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
+				break;
+			}
+			i++; //step 4
+		}
+
+		
 }
 
 int main();
